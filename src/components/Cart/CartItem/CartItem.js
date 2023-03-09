2bd58700238395f967b000e3b15c403a4042a980
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2023-03-09 12:46:38 +0100

    Revert "Revert "8/4/2022 commit""
    This reverts commit 3e1d27dc029bf8133ccf6f1a2d28b0453d2f9d40.

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, Button } from '@material-ui/core';
 import useStyles from './styles';
-const CartItem = ({item}) => {
+const CartItem = ({item,handleUpdateQty,removeItem}) => {
     const classes = useStyles();
   return (
       <Card>
@@ -12,11 +12,11 @@ const CartItem = ({item}) => {
           </CardContent>
           <CardActions className={classes.cartActions}>
               <div className={classes.buttons}>
-                  <Button type="button" size='small'>+</Button>
+                  <Button type="button" size='small' onClick={()=>handleUpdateQty(item.id,item.quantity+1)}>+</Button>
                   <Typography>{item.quantity}</Typography>
-                  <Button type="button" size='small'>-</Button>
+                  <Button type="button" size='small' onClick={()=>handleUpdateQty(item.id,item.quantity-1)}>-</Button>
               </div>
-              <Button variant='contained' type='button' color='secondary'>Remove</Button>
+              <Button variant='contained' type='button' color='secondary'  onClick={()=>removeItem(item.id)}>Remove</Button>
           </CardActions>
       </Card>
   )
---
+++ src/components/CheckForm/AddressForm.js
@@ -0,0 +1,96 @@
+import React, { useEffect,useState } from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import FormInput from './CustomTextField';
+import { commerce } from '../../lib/commerce';
+const AddressForm = ({checkoutToken,next}) => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions,setShippingSubdivisions] = useState([])
+    const [shippingSubdivision,setShippingSubdivision] = useState('')
+    const [shippingOptions,setShippingOptions] = useState([])
+    const [shippingOption,setShippingOption] = useState('')
+    const methods = useForm();
+
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const {countries} = await commerce.services.localeListShippingCountries(checkoutTokenId);
+     
+        setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0]);
+    }
+
+    const fetchShippingSubdivisions = async (countryCode) => {
+        const {subdivisions} = await commerce.services.localeListSubdivisions(countryCode);
+        
+        setShippingSubdivisions(subdivisions);
+        setShippingSubdivision(Object.keys(subdivisions)[0]);
+    }
+    const fetchShippingOptions = async (checkoutTokenId, country, region=null) => {
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region }) 
+        console.log(options);
+          setShippingOptions(options);
+        setShippingOption(options[0].id);
+   }
+    useEffect(() => {
+        fetchShippingSubdivisions(shippingCountry);
+   },[shippingCountry])
+  
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id);
+    }, [])
+    useEffect(() => {
+          fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision);
+    },[shippingSubdivision])
+    return (
+      <>
+            <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit((data)=>next({...data,shippingCountry,shippingOption,shippingSubdivision}))}>
+                    <Grid container spacing={3}>
+                    <FormInput required name='firstName' label='First Name'/>
+                        <FormInput name='lastName' label='Last Name' />
+                        <FormInput name='address' label='Address'/>
+                        <FormInput name='email' label='Email'/>
+                        <FormInput name='city' label='City' />
+                        <FormInput name='zip' label='ZIP / Postal code' />
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {Object.entries(shippingCountries).map((item,i) => (
+                                    <MenuItem key={i} value={item[0]}>{item[1]}</MenuItem>
+                                ))}
+                             
+                            </Select>
+                        </Grid>
+                         <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping SubDivision</InputLabel>
+                            <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                              {Object.entries(shippingSubdivisions).map((item,i) => (
+                                    <MenuItem key={i} value={item[0]}>{item[1]}</MenuItem>
+                                ))}
+                            </Select>
+                        </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value={shippingOption} fullWidth onChange={(e) => setShippingOption(e.target.value)}>
+                              {shippingOptions.map((item,i) => (
+                                    <MenuItem key={i} value={item.id}>{`${item.description} - ${item.price.formatted_with_symbol}`}</MenuItem>
+                                ))}
+                            </Select>
+                        </Grid>
+                    </Grid>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Button component={Link} to='/cart' variant='outlined'>Back to Cart</Button>
+                      <Button type='submit' variant='contained' color='primary'>Next</Button>
+                </div>
+              
+                </form>
+              
+            </FormProvider>
+  </>
+            )
+}
+
+export default AddressForm
