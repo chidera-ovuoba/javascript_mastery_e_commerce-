3e1d27dc029bf8133ccf6f1a2d28b0453d2f9d40
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2023-03-09 12:43:56 +0100

    Revert "8/4/2022 commit"
    This reverts commit b3ab274e035ebe5858298cd97ea8b865257c4697.

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, Button } from '@material-ui/core';
 import useStyles from './styles';
-const CartItem = ({item,handleUpdateQty,removeItem}) => {
+const CartItem = ({item}) => {
     const classes = useStyles();
   return (
       <Card>
@@ -12,11 +12,11 @@ const CartItem = ({item,handleUpdateQty,removeItem}) => {
           </CardContent>
           <CardActions className={classes.cartActions}>
               <div className={classes.buttons}>
-                  <Button type="button" size='small' onClick={()=>handleUpdateQty(item.id,item.quantity+1)}>+</Button>
+                  <Button type="button" size='small'>+</Button>
                   <Typography>{item.quantity}</Typography>
-                  <Button type="button" size='small' onClick={()=>handleUpdateQty(item.id,item.quantity-1)}>-</Button>
+                  <Button type="button" size='small'>-</Button>
               </div>
-              <Button variant='contained' type='button' color='secondary'  onClick={()=>removeItem(item.id)}>Remove</Button>
+              <Button variant='contained' type='button' color='secondary'>Remove</Button>
           </CardActions>
       </Card>
   )
